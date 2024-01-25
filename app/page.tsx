a189730aafd2e81688bfafbe565ce95333947aca
William Giammona <wgiammona@gmail.com>
2024-01-25 18:13:09 +0200

    created gradient

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Head>
-      <title>Netflix Clone</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <>
+      <Head>
+        <title>Netflix Clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]"></div>
+    </>
   );
 }
