6fb876b9a5090316ad53f06e0eefead0da50f350
William Giammona <wgiammona@gmail.com>
2024-01-25 14:23:34 +0200

    updated image

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,13 +5,12 @@ function Header() {
   return (
     <header className="w-screen ">
       <nav className="container flex justify-between m-5 ">
-        <figure>
+        <figure className="object-contain cursor-pointer">
           <Image
-            src="https://rb.gy/ulxxee"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
             width={100}
             height={100}
             alt="logo"
-            className="object-contain cursor-pointer"
           />
         </figure>
         <ul className="flex space-x-2">
@@ -20,6 +19,12 @@ function Header() {
           </li>
           <li>
             <Link href="/">About</Link>
+          </li>
+          <li>
+            <Link href="/">Contact</Link>
+          </li>
+          <li>
+            <Link href="/">Contact</Link>
           </li>
           <li>
             <Link href="/">Contact</Link>
