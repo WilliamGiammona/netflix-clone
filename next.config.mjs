2267848d44fb26c421ca9579223f3e11f3e6e328
William Giammona <wgiammona@gmail.com>
2024-01-25 16:35:49 +0200

    added image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["occ-0-1190-2774.1.nflxso.net"],
+  },
+};
 
 export default nextConfig;
